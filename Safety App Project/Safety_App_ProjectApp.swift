c8310e4c312b4338cbfdc8a96dd2b51a6c9a347d
Dhruv Dingari <ddingari@hotmail.com>
2021-02-24 14:04:01 -0500

    Initial Commit

---
+++ Safety App Project/Safety_App_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Safety_App_ProjectApp.swift
+//  Safety App Project
+//
+//  Created by Dhruv Dingari on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Safety_App_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
